ac49e67c91a0cd41f8c35b9689d2be37f9676498
Yusuf Şahin <yusufsahin429@gmail.com>
2020-08-31 04:03:51 +0300

    done

--- module5-soln/js/script.js
+++ module5-soln/js/script.js
@@ -116,7 +116,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,
-        "randomCategoryShortName", 'chosenCategoryShortName');
+        'randomCategoryShortName', chosenCategoryShortName);
       console.log(homeHtmlToInsertIntoMainPage);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
